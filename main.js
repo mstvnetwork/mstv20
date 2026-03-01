1c2971f71878bbd2f6a1c962758789d6d7f146ab
mstvnetwork <mstvindianetworkusa@gmail.com>
2026-03-02 01:21:59 +1100

    use public cors proxy

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-// The proxied URL pointing to your Netlify redirect
-const streamUrl = '/shemaroo/smil:shemarootvadp.smil/playlist.m3u8&id=2092&type=mp4';
+// The Public CORS Proxy + Your Stream URL
+const proxy = 'https://cors-anywhere.herokuapp.com';
+const targetUrl = 'https://airtelapp.shemaroo.com';
+const streamUrl = proxy + targetUrl;
 
-const video = document.getElementById('video'); // Make sure your index.html has <video id="video"></video>
+const video = document.getElementById('video');
 
 if (Hls.isSupported()) {
-    const hls = new Hls({
-        xhrSetup: function (xhr, url) {
-            xhr.withCredentials = false; // Required for some proxied streams
-        }
-    });
+    const hls = new Hls();
     hls.loadSource(streamUrl);
     hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
         video.play();
     });
-} 
-// For Safari/iOS which has native HLS support
-else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
     video.src = streamUrl;
-    video.addEventListener('loadedmetadata', function () {
-        video.play();
-    });
 }
 
