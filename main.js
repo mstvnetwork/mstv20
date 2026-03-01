c9870d96d823f50a4d053acd223ba2e7ae19328a
mstvnetwork <mstvindianetworkusa@gmail.com>
2026-03-02 01:43:33 +1100

    fix: update index and main with proxy logic

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-// The Public CORS Proxy + Your Stream URL
-const proxy = 'https://cors-anywhere.herokuapp.com';
-const targetUrl = 'https://airtelapp.shemaroo.com';
-const streamUrl = proxy + targetUrl;
+// This URL uses your working Netlify proxy path
+const streamUrl = '/shemaroo/smil:shemarootvadp.smil/playlist.m3u8&id=2092&type=mp4';
 
+// Target the video element in your index.html
 const video = document.getElementById('video');
 
-if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(streamUrl);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function() {
-        video.play();
-    });
-} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = streamUrl;
+function initPlayer() {
+    if (Hls.isSupported()) {
+        const hls = new Hls({
+            // Enables cross-domain credentials if Shemaroo requires cookies
+            xhrSetup: function (xhr) {
+                xhr.withCredentials = false;
+            }
+        });
+        
+        hls.loadSource(streamUrl);
+        hls.attachMedia(video);
+        
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            console.log("Stream manifest loaded via Proxy. Playing...");
+            video.play().catch(e => console.error("Autoplay blocked:", e));
+        });
+
+        hls.on(Hls.Events.ERROR, function (event, data) {
+            if (data.fatal) {
+                console.error("HLS Error:", data.type);
+                hls.recoverMediaError();
+            }
+        });
+    } 
+    // Native support for Safari/iOS
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = streamUrl;
+        video.addEventListener('loadedmetadata', function () {
+            video.play();
+        });
+    }
+}
+
+// Start the player
+if (video) {
+    initPlayer();
+} else {
+    console.error("Could not find <video id='video'> in your index.html");
 }
 
